3de5ba17b886a7d927ee85155e8c28b361322cf2
pepepepepei <jumpei.yamada.nagoya@gmail.com>
2023-05-05 06:17:19 +0900

    titleタグを設定した

--- src/components/base/Layout/Layout.jsx
+++ src/components/base/Layout/Layout.jsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import SideBar from "../SideBar/SideBar";
@@ -6,6 +7,13 @@ import styles from "./Layout.module.css";
 export default function Layout({ title, children }) {
   return (
     <>
+      <Head>
+        <title>
+          {title}
+          {title ? "｜" : ""}
+          Aisai Marching Band BLOWING
+        </title>
+      </Head>
       <SideBar className={styles.sideBar} />
       <main className={styles.main}>
         <Header title={title} />
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 import Layout from "@/components/base/Layout/Layout";
 
 export default function Home() {
-  return <Layout title="トップ"></Layout>;
+  return <Layout></Layout>;
 }
