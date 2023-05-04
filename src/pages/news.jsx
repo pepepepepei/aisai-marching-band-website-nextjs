2ce61c4a6727b7af8fa951af4141f81fea0aae72
pepepepepei <jumpei.yamada.nagoya@gmail.com>
2023-05-05 03:31:54 +0900

    新規ページ類を作成した

---
+++ src/pages/news.jsx
@@ -0,0 +1,5 @@
+import Layout from "@/components/base/Layout/Layout";
+
+export default function Home() {
+  return <Layout title="お知らせ"></Layout>;
+}
---
+++ src/pages/privacy.jsx
@@ -0,0 +1,5 @@
+import Layout from "@/components/base/Layout/Layout";
+
+export default function Home() {
+  return <Layout title="当サイトについて"></Layout>;
+}
