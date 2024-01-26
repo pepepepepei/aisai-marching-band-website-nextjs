6bd246499dba21490c81cff474f37d50590fa2da
pepepepepei <jumpei.yamada.nagoya@gmail.com>
2024-01-26 22:30:08 +0900

    ハンバーガーメニューを実装した

--- src/components/base/SideBar/SideBar.jsx
+++ src/components/base/SideBar/SideBar.jsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import styles from "./SideBar.module.scss";
 
 export default function SideBar() {
+  const [openMenu, setOpenMenu] = useState(false);
+  const handleMenuOpen = () => {
+    setOpenMenu(!openMenu);
+  };
+  const handleMenuClose = () => {
+    setOpenMenu(false);
+  };
+
   const router = useRouter();
   const navItems = [
     {
@@ -47,38 +56,38 @@ export default function SideBar() {
         </div>
       </Link>
 
-      {/* <button
+      <button
         type="button"
-        title="メニューを開く"
-        class="menu-btn"
+        title={`メニューを${openMenu ? "閉じる" : "開く"}`}
+        className={[styles.menuBtn, openMenu ? styles.open : ""].join(" ")}
         aria-controls="menuContent"
-        aria-expanded="false"
+        aria-expanded={openMenu}
+        onClick={handleMenuOpen}
       >
-        <span class="menu-btn-open">MENU</span>
-        <span class="menu-btn-close">CLOSE</span>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </button> */}
+        <span className={styles.menuBtnText}>{openMenu ? "CLOSE" : "MENU"}</span>
+        <span
+          className={[styles.menuBtnHamburgerLine, openMenu ? styles.open : ""].join(" ")}
+        ></span>
+      </button>
 
-      <nav className={styles.menuContent}>
+      <nav id="menuContent" className={[styles.menuContent, openMenu ? styles.open : ""].join(" ")}>
         <ul className={styles.menuContentList}>
           {navItems.map((item) => {
             return (
               <li key={item.href} className={styles.menuContentListItem}>
                 <Link
+                  onClick={handleMenuClose}
                   href={item.href}
-                  className={
+                  className={[
+                    styles.menuContentListItemLink,
                     (
                       item.href === "/"
                         ? router.pathname === item.href
                         : router.pathname.startsWith(item.href)
                     )
-                      ? styles.menuContentListItemLinkCurrent
-                      : styles.menuContentListItemLink
-                  }
+                      ? styles.current
+                      : "",
+                  ].join(" ")}
                 >
                   {item.title}
                 </Link>
