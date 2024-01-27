34f0b0589cc9aec3cfcd4b284a1e815b63c6b189
pepepepepei <jumpei.yamada.nagoya@gmail.com>
2024-01-27 21:34:03 +0900

    概要ページを作った

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,5 +1,26 @@
 import Layout from "@/components/base/Layout/Layout";
 
 export default function Home() {
-  return <Layout title="概要"></Layout>;
+  return (
+    <Layout title="概要">
+      <section>
+        <h2>バンド理念</h2>
+        <p>
+          私達は、マーチング・演奏活動を通して「喜び」や「感動」を「共感」したいと考えています。
+          <br />
+          <br />
+          昨今では、楽器が吹きたいけどできない、マーチングがやりたいけどできない・・・
+          <br />
+          <br />
+          皆様の様々なくすぶった「想い」、心のどこかにある「夢」を実現できたらと考えています。
+          <br />
+          <br />
+          「夢」を実現し、音楽を通し地域貢献に努めます！
+          <br />
+          <br />
+          団長　戸村拓哉
+        </p>
+      </section>
+    </Layout>
+  );
 }
